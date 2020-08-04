6b19780af9a54b5d0185fde9b5d510f991230025
gstark0 <gstark0@icloud.com>
2020-08-04 22:07:02 +0200

    Add search bar

--- StockDesk/AddStock.swift
+++ StockDesk/AddStock.swift
@@ -1,7 +1,34 @@
 import SwiftUI
 
 struct AddStock: View {
+    @State private var query = ""
+    @State private var isEditing = false
+    
     var body: some View {
-        Text("Add a stock")
+        HStack {
+            TextField("Search by symbol", text: $query)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(6)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            
+            if isEditing {
+                Button("Cancel") {
+                    self.isEditing = false
+                    self.query = ""
+                }
+                .padding(.trailing, 10)
+            }
+        }
+    }
+}
+
+struct AddStock_Previews: PreviewProvider {
+    static var previews: some View {
+        AddStock()
     }
 }
